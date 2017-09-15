b3879ad28878296da5e6d9b2826b2efe1b678245
jhatch <jhatch@shopathome.com>
2017-09-15 11:26:44 -0600

    added flux architecture loop

---
+++ src/actions/userActions.js
@@ -0,0 +1,15 @@
+import dispatcher from './dispatcher';
+
+export default class Actions {
+    addUser: (userInfo) => {
+
+    },
+
+    updateUser: (newInfo) => {
+
+    },
+
+    deleteUser: (userId) => {
+      
+    }
+}
--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
-
+import UserTile from './userTile';
 
 
 export default class App extends React.Component {
-  render = () => {
-    return (
-      <div className="app">
-        HEY THERE
-      </div>
-    );
-  }
+    constructor() {
+        super();
 
+        this.state = {};
+    }
+
+
+
+    render = () => {
+        var userList = data.map(function(profile, index){
+            return <UserTile key={ index } profileData={ profile }/>;
+        })
+
+        return (
+          <div className="app">
+
+          </div>
+        );
+    }
 }
